e2553968a0f78860ace8d9d783741ce8850ed1b6
Bustamante Carlos G <bustamantec@agjs.gov.ar>
2024-10-21 13:15:34 -0300

    Finalizado Practico_04 Servidor Node MVC

---
+++ Practico_04_Super/persistence/superheroesDataSource.mjs
@@ -0,0 +1,9 @@
+export default class SuperheroesDataSource {    
+    obtenerTodas(){
+        throw new Error("Este metodo debe ser implementado por la subclase");         
+    }
+
+    guardar(){
+        throw new Error("Este metodo debe ser implementado por la subclase");       
+    }
+}
---
+++ Practico_04_Super/persistence/superheroesRepository.mjs
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+import SuperheroesDataSource from './superheroesDataSource.mjs';
+
+export default class SuperheroesFileRepository extends SuperheroesDataSource {
+    constructor(){
+        super();
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);        
+        this.filePath = path.join(__dirname, '../superheroes.txt');        
+    }
+
+    obtenerTodos(){
+        const data = fs.readFileSync(this.filePath, 'utf-8');
+        return JSON.parse(data);
+    }
+
+    guardar(superheroes){
+        fs.writeFileSync(this.filePath, JSON.stringify(superheroes, null, 2));
+    }
+}
